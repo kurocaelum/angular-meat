b48cfe0a783a445cdb9f9a2ad87951e22ad7dbc2
Victor <victor_brandao@outlook.com>
2021-06-09 06:37:47 -0300

    Menu itens e Pipe currency

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import { ErrorHandler } from '../app.error-handler'
 
 import { Restaurant } from "./restaurant/restaurant.model"
 import { MEAT_API } from "../app.api"
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model"
 
 @Injectable()
 export class RestaurantsService {
@@ -29,6 +30,12 @@ export class RestaurantsService {
 
   reviewsOfRestaurant(id: string): Observable<any> {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
