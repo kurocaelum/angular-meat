cc578f0c03ec5a56a2bc6549a12c1d5201f9dbb6
Victor <victor_brandao@outlook.com>
2021-06-09 06:31:11 -0300

    Refactoring

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { RestaurantsService } from 'app/restaurants/restaurants.service';
-import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { RestaurantsService } from 'app/restaurants/restaurants.service'
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'mt-reviews',
