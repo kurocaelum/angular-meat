5e9ca068b798fdb7f1cfb2d3325d1ed966772162
Victor <victor_brandao@outlook.com>
2021-06-12 15:19:48 -0300

    Ligando shopping cart ao componente order

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'mt-order',
@@ -13,9 +15,27 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão refeição', value: 'REF' }
   ]
 
-  constructor() { }
+  constructor(private orderService: OrderService) {
+
+  }
 
   ngOnInit() {
+  }
+
+  cartItems() {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
   }
 
 }
--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -12,9 +12,20 @@ export class ShoppingCartService {
     let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
 
     if (foundItem) {
-      foundItem.quantity++
+      this.increaseQty(foundItem)
     } else {
       this.items.push(new CartItem(item))
+    }
+  }
+
+  increaseQty(item: CartItem) {
+    item.quantity++
+  }
+
+  decreaseQty(item: CartItem) {
+    item.quantity--
+    if(item.quantity === 0) {
+      this.removeItem(item)
     }
   }
 
