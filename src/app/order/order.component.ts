2dee482a1f555b87a9c62739ffe3c31668f98265
Victor <victor_brandao@outlook.com>
2021-06-12 15:58:03 -0300

    Componente de total de compra (com frete)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -9,6 +9,8 @@ import { OrderService } from './order.service';
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 8
+
   paymentOptions: RadioOption[] = [
     { label: 'Dinheiro', value: 'MON' },
     { label: 'Cartão de crédito', value: 'DEB' },
@@ -20,6 +22,10 @@ export class OrderComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue()
   }
 
   cartItems() {
